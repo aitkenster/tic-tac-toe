7f05e059a447b0ce3d5cd8f982b3e93b8509946c
Nicola Aitken <naitken22@gmail.com>
2014-08-20 19:19:44 +0100

    Will not allow pieces to be placed in spaces already in use

--- jasmine/spec/ticTacToeSpec.js
+++ jasmine/spec/ticTacToeSpec.js
@@ -11,6 +11,16 @@ describe('tic-tac-toe', function(){
 	it("lets the player place a O on the grid", function(){
 		game.placeNought(1);
 		expect(game.Grid[1]).toEqual('O');
-		expect(game.Grid[2]).toEqual(' ');
 	});
+
+	it("lets the computer place a X on the grid", function(){
+		game.placeCross(1);
+		expect(game.Grid[1]).toEqual('X')
+	})
+
+	it("won't let the player place a O on a square already in use", function(){
+		game.placeCross(1);
+		game.placeNought(1);
+		expect(game.Grid[1]).toEqual('X')
+	})
 });
--- jasmine/src/ticTacToe.js
+++ jasmine/src/ticTacToe.js
@@ -1,12 +1,24 @@
 function Game(){
-	this.Grid = [' ', ' ', ' ',
-							 ' ', ' ', ' ',
-							 ' ', ' ', ' '];
+	this.Grid = ['', '', '',
+							 '', '', '',
+							 '', '', ''];
 };
 
 Game.prototype.placeNought = function(position){
-	for(var i=0; i<this.Grid.length;i++ ){
-	if (this.Grid[i]==this.Grid[position])
-		this.Grid.splice(i,1,'O');
+	this.placePiece(position, 'O');
+}
+
+Game.prototype.placeCross = function(position){
+	this.placePiece(position, 'X');
+}
+
+Game.prototype.placePiece = function(position, piece){
+	if (this.Grid[position] === '' ){
+		for(var i=0; i<this.Grid.length;i++ ){
+			if (this.Grid[i]==this.Grid[position]){
+				this.Grid.splice(i,1,piece);
+			}
+		}
 	}
-};
+
+}
