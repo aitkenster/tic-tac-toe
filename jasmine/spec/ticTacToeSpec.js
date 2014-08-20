ffd93225f5eaf13c656aaa383d02e4e515b3f637
Nicola Aitken <naitken22@gmail.com>
2014-08-20 19:03:07 +0100

    Lets you place a O in the grid

--- jasmine/spec/ticTacToeSpec.js
+++ jasmine/spec/ticTacToeSpec.js
@@ -7,4 +7,10 @@ describe('tic-tac-toe', function(){
 		expect(game.Grid).not.toContain('X');
 		expect(game.Grid).not.toContain('O');
 	});
+
+	it("lets the player place a O on the grid", function(){
+		game.placeNought(1);
+		expect(game.Grid[1]).toEqual('O');
+		expect(game.Grid[2]).toEqual(' ');
+	});
 });
--- jasmine/src/ticTacToe.js
+++ jasmine/src/ticTacToe.js
@@ -4,3 +4,9 @@ function Game(){
 							 ' ', ' ', ' '];
 };
 
+Game.prototype.placeNought = function(position){
+	for(var i=0; i<this.Grid.length;i++ ){
+	if (this.Grid[i]==this.Grid[position])
+		this.Grid.splice(i,1,'O');
+	}
+};
